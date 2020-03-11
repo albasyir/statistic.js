a8c8b120fcf75fd2df87115dfbf671ced55e0cab
Abdul Aziz Al Basyir <abdulazizalbasyir119@gmail.com>
2020-03-11 11:52:55 +0700

    add syntax testing

--- test/index.js
+++ test/index.js
@@ -1,2 +1,10 @@
-require('./state_array_test');
-require('./state_object_test');
+const testFile = ['./state_array_test.js', './state_object_test.js'];
+
+testFile.forEach((file) => {
+    console.log("================ Testing : " + file + "======================\n");
+    require(file);
+    console.log("================ Testied : " + file + "======================\n\n");
+    
+    console.log("-");
+    console.log("-");
+})
--- test/state_array_test.js
+++ test/state_array_test.js
@@ -1,20 +1,20 @@
-const { Statistic } = require('../dist/Statistic')
+const { Statistic } = require('../index')
 
 const statistic = new Statistic([10, 30, 10, 30, 10, 20]);
 console.log('construction : ', statistic.getState());
 
 statistic.setState([10, 20, 30]);
-console.log('setter state : ', statistic.getState());
+console.log('statistic.setState([10, 20, 30]) : ', statistic.getState());
 
 statistic.pushState([40, 50, 60]);
-console.log('push state : ', statistic.getState());
+console.log('statistic.pushState([40, 50, 60]) : ', statistic.getState());
 
 statistic.pushState([40, 50, 60, 10, 20, 30]);
 statistic.popStateBetween(30, 60);
-console.log('pop between 30 and 60 : ', statistic.getState());
+console.log('statistic.popStateBetween(30, 60) : ', statistic.getState());
 
 statistic.sortState();
-console.log('sort : ', statistic.getState());
+console.log('statistic.sortState() : ', statistic.getState());
 
 statistic.reverseState();
-console.log('reverse : ', statistic.getState());
+console.log('statistic.reverseState() : ', statistic.getState());
